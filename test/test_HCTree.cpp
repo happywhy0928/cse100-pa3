1e7c4a5a808a5116d8b39221f9d027c7bd2f1dc2
happywhy0928 <56148369+happywhy0928@users.noreply.github.com>
2019-11-06 11:18:20 -0800

    Add files via upload

--- test/test_HCTree.cpp
+++ test/test_HCTree.cpp
@@ -40,6 +40,18 @@ class ComplexHCTreeFixture : public ::testing::Test {
         tree1.build(freqs);
     }
 };
+class SingleHCTreeFixture : public ::testing::Test {
+  protected:
+    HCTree tree1;
+
+  public:
+    SingleHCTreeFixture() {
+        // initialization code here
+        vector<unsigned int> freqs(256);
+        freqs['A'] = 2;
+        tree1.build(freqs);
+    }
+};
 TEST_F(SimpleHCTreeFixture, TEST_ENCODE) {
     ostringstream os;
     tree.encode('a', os);
@@ -72,4 +84,44 @@ TEST_F(ComplexHCTreeFixture, TEST_DECODE1) {
 TEST_F(ComplexHCTreeFixture, TEST_DECODE2) {
     istringstream is("0010");
     ASSERT_EQ(tree1.decode(is), 'A');
+}
+TEST_F(SingleHCTreeFixture, TEST_ENCODE4) {
+    ostringstream os;
+    tree1.encode('A', os);
+    ASSERT_EQ(os.str(), "0");
+}
+TEST_F(SingleHCTreeFixture, TEST_DECODE3) {
+    istringstream is("0");
+    ASSERT_EQ(tree1.decode(is), 'A');
+}
+class DoubleHCTreeFixture : public ::testing::Test {
+  protected:
+    HCTree tree1;
+
+  public:
+    DoubleHCTreeFixture() {
+        // initialization code here
+        vector<unsigned int> freqs(256);
+        freqs['A'] = 2;
+        freqs['B'] = 2;
+        tree1.build(freqs);
+    }
+};
+TEST_F(DoubleHCTreeFixture, TEST_ENCODE4) {
+    ostringstream os;
+    tree1.encode('A', os);
+    ASSERT_EQ(os.str(), "1");
+}
+TEST_F(DoubleHCTreeFixture, TEST_ENCODE5) {
+    ostringstream os;
+    tree1.encode('B', os);
+    ASSERT_EQ(os.str(), "0");
+}
+TEST_F(DoubleHCTreeFixture, TEST_DECODE4) {
+    istringstream is("1");
+    ASSERT_EQ(tree1.decode(is), 'A');
+}
+TEST_F(DoubleHCTreeFixture, TEST_DECODE5) {
+    istringstream is("0");
+    ASSERT_EQ(tree1.decode(is), 'B');
 }
